5704e4e1bb07db947959db48add1b28376b4f1ef
dietrich313 <dietrich313@gmail.com>
2018-03-01 19:40:53 +0200

    313

---
+++ Assets/Sources/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D playerRB2D;
+
+	
+	void Start () {
+        transform.position = new Vector2(-Screen.width, Screen.height);
+        playerRB2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		
+	}
+}
